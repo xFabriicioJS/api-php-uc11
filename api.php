997ecbb6b20266f7c7e08d63256a6e6e67cc2bb1
xFabriicioJS <fabriciocostamonteiro@hotmail.com>
2022-09-06 11:46:37 -0300

    aula terca

--- api.php
+++ api.php
@@ -2,5 +2,42 @@
 //Api - aplicação para recursos de app mobile
     
 
+include_once('conn.php');
+
+//variável que recebe o conteúdo da requisição do App decodificando-a
+
+$postjson = json_decode(file_get_contents('php://input', true), true);
+
+if($postjson['requisicao'] == 'add'){
+    $query = $pdo->prepare("insert into usuarios set nome = :nome, usuario = :usuario, senha=:senha, senha_original=:senha_original, nivel=:nivel, ativo=1");
+
+    $query->bindValue(":nome", $postjson['nome']);
+    $query->bindValue(":usuario", $postjson['usuario']);
+    $query->bindValue(":senha", md5($postjson['senha']));
+    $query->bindValue(":senha_original", $postjson['senha']);
+    $query->bindValue(":nivel", $postjson['nivel']);
+    $query->execute();
+
+    $id = $pdo->lastInsertId();
+
+    if($query){
+        $result =  json_encode(array('success' => true, 'id' => $id));
+    }else{
+        $result = json_encode(array('success' => false, 'msg'=> 'Falha ao inserir o usuário'));
+    }
+
+    echo $result;
+
+}
+
+
+// Final requisição add
+
+    else if($postjson['requisicao']=='listar'){
+        if($postjson['nome'] == ''){
+            $query = $pdo->query("SELECT * FROM usuario order BY id desc limit $postjson['start'], $postjson['limit']")
+        }
+    }
+
 
 ?>
--- conn.php
+++ conn.php
@@ -3,4 +3,37 @@
 //conexão com banco de dados da app mobile ionic
 
 
+//Configurações para as requisições JSON
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+header('Content-Type: application/json; charset=utf-8');
+
+// Dados do servidor local
+$banco =  'controledb';
+$host =  'localhost'; //127.0.0.1
+$usuario = 'root';
+$senha = '';
+
+try{
+
+    $pdo = new PDO("mysql:dbname-$banco;
+    host=$host;
+    ", "$usuario", "$senha = ''");
+
+}catch(Exception $e){
+    echo 'Erro ao conectar com ';
+}
+
+
+//Dados do servidor remoto/hospedado
+// $banco =  'wellington_91';
+// $host =  'softkleen.com.br'; //127.0.0.1
+// $usuario = 'apiti9111';
+// $senha = '';
+
+
 ?>
